08d7a40f4aacbb8d74235fbc4bf1296cafcff6a8
徐欲东 <xuyd22082203@q1.com>
2024-03-11 11:30:54 +0800

    添加项目文件。

---
+++ leetcodeAnswer/Program.cs
@@ -0,0 +1,31 @@
+﻿#region 将标题首字母大写
+Console.WriteLine(CapitalizeTitle("L hV")); ;
+static string CapitalizeTitle(string title)
+{
+    var chars = title.ToCharArray();
+    int left = 0,right = 0;
+    while (right < title.Length)
+    {
+        while (right < title.Length && title[right] != ' ')
+        {
+            ++right;
+        }
+
+        if(right - left > 2)
+        {
+            chars[left] = char.ToUpper(title[left]);
+            ++left;
+        }
+
+        while(left < right)
+        {
+            chars[left] = char.ToLower(chars[left]);
+            ++left;
+        }
+        left = right + 1;
+        ++right;
+    }
+
+    return new string(chars);
+}
+#endregion
